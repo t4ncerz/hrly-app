6c0f9fe4b1d92c6688ceca1a8d838dd181b8abb2
Szymon Pancerz <goodsite.szymon@gmail.com>
2025-03-27 09:48:52 +0100

    feat: adjust connection pooling

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,11 +1,13 @@
 import { env } from "@/data/env/server";
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
-import postgres from "postgres";
-
-const client = postgres(env.POSTGRES_URL_NON_POOLING);
 
 export const db = drizzle({
   schema,
-  client,
+  connection: {
+    password: env.POSTGRES_PASSWORD,
+    user: env.POSTGRES_USER,
+    database: env.POSTGRES_DATABASE,
+    host: env.POSTGRES_HOST,
+  },
 });
