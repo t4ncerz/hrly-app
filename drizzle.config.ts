b96ad5fe38b033a9e315cc445cdf6b7a920efc78
Szymon Pancerz <goodsite.szymon@gmail.com>
2025-03-27 09:41:18 +0100

    feat: adjust drizzle integration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,13 +5,7 @@ export default defineConfig({
   out: "./src/drizzle/migrations",
   schema: "./src/drizzle/schema.ts",
   dialect: "postgresql",
-  strict: true,
-  verbose: true,
   dbCredentials: {
-    password: env.POSTGRES_PASSWORD,
-    user: env.POSTGRES_USER,
-    database: env.POSTGRES_DATABASE,
-    host: env.POSTGRES_HOST,
     url: env.POSTGRES_URL,
   },
 });
--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,13 +1,11 @@
 import { env } from "@/data/env/server";
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
+import postgres from "postgres";
+
+const client = postgres(env.POSTGRES_URL);
 
 export const db = drizzle({
   schema,
-  connection: {
-    password: env.POSTGRES_PASSWORD,
-    user: env.POSTGRES_USER,
-    database: env.POSTGRES_DATABASE,
-    host: env.POSTGRES_HOST,
-  },
+  client,
 });
