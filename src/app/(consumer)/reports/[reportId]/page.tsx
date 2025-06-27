3ef1f4609e61ce8bc5a2f544580e7921f829813f
Szymon Pancerz <goodsite.szymon@gmail.com>
2025-06-27 22:23:19 +0200

    feat: mv rreport to correct dir

--- src/app/(consumer)/reports/[reportId]/page.tsx
+++ src/app/(consumer)/reports/[reportId]/page.tsx
@@ -5,17 +5,17 @@ import { redirect } from "next/navigation";
 export default async function ReportDetailPage({
   params,
 }: {
-  params: Promise<{ id: string }>;
+  params: Promise<{ reportId: string }>;
 }) {
-  const { id } = await params;
+  const { reportId } = await params;
 
-  if (!id) {
+  if (!reportId) {
     redirect("/login");
   }
 
   return (
     <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-screen-xl mx-auto">
-      <ReportDisplay reportId={id} />
+      <ReportDisplay reportId={reportId} />
     </div>
   );
 }
